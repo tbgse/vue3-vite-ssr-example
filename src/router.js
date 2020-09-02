530f530f652b8a5ff1e065c26eb90a4fe5e68c66
Tobias Guse <tobe.guse@gmail.com>
2020-09-02 14:47:51 +0200

    remove async component usage

--- src/router.js
+++ src/router.js
@@ -1,4 +1,3 @@
-import { defineAsyncComponent } from "vue"
 import { createRouter, createMemoryHistory } from 'vue-router'
 
 export default function (type) {
@@ -7,9 +6,9 @@ export default function (type) {
   return createRouter({
     history: routerHistory,
     routes: [
-      { path: '/', component: defineAsyncComponent(() => import("./components/Homepage.vue")), props: true },
-      { path: '/a', component: defineAsyncComponent(() => import("./components/PageA.vue")), props: true },
-      { path: '/b', component: defineAsyncComponent(() => import("./components/PageB.vue")), props: true },
+      { path: '/', component: () => import("./components/Homepage.vue"), props: true },
+      { path: '/a', component: () => import("./components/PageA.vue"), props: true },
+      { path: '/b', component: () => import("./components/PageB.vue"), props: true },
     ]
   });
 }
